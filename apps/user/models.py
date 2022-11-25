1e5ace0ab7baaa765346619ec524b26444c523b5
HyeonsooKim <gustn9442@naver.com>
2022-11-25 14:45:35 +0900

    chore: 오타 수정

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,6 +1,8 @@
 # apps/user/models.py
 from django.db import models
 from django.utils import timezone
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 
